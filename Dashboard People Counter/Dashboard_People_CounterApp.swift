cde87b622ba177817d586dff53eac1745574343f
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-07-13 15:43:34 +0700

    Initial Commit

---
+++ Dashboard People Counter/Dashboard_People_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_People_CounterApp.swift
+//  Dashboard People Counter
+//
+//  Created by Rainer Regan on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_People_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
